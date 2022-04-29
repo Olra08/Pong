e489fe6c3daf952f36da84d564302a116c660b0b
Fabrizzio Sima <cafasicha@hotmail.com>
2022-04-28 21:43:10 -0500

    tercero

--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public enum TipoJugador
 {
-    JUGADOR1, JUGADOR2
+    JUGADOR1, JUGADOR2, CPU
 }
 
 public class MovementManager : MonoBehaviour
 {
     public float speed;
+    private float speedAI = 9.5f;
     public TipoJugador tipo;
+
+    public GameObject ball;
+    public Vector2 ballPos;
+
+    private bool dPressed = false;
 
     private void Update()
     {
@@ -16,17 +22,70 @@ public class MovementManager : MonoBehaviour
         if (tipo == TipoJugador.JUGADOR1)
         {
             movement = Input.GetAxis("Vertical");
+            Vector3 actualPos = transform.position;
+            transform.position = new Vector3(
+                actualPos.x,
+                Mathf.Clamp(actualPos.y + (speed * movement * Time.deltaTime), -7f, 7f),
+                actualPos.z
+            );
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                dPressed = true;
+            } else if (Input.GetKeyUp(KeyCode.D))
+            {
+                dPressed = false;
+            }
+            if (dPressed)
+            {
+                
+            }
         }
-        else
+        if (tipo == TipoJugador.JUGADOR2)
         {
             movement = Input.GetAxis("Vertical2");
+            Vector3 actualPos = transform.position;
+            transform.position = new Vector3(
+                actualPos.x,
+                Mathf.Clamp(actualPos.y + (speed * movement * Time.deltaTime), -7f, 7f),
+                actualPos.z
+            );
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                tipo = TipoJugador.CPU;
+            }
         }
-        Vector3 actualPos = transform.position;
-        transform.position = new Vector3(
-            actualPos.x,
-            Mathf.Clamp(actualPos.y + (speed * movement * Time.deltaTime), -7f, 7f),
-            actualPos.z
-        );
+        if (tipo == TipoJugador.CPU)
+        {
+            Move();
+            if (Input.GetKeyDown(KeyCode.RightControl))
+            {
+                tipo = TipoJugador.JUGADOR2;
+            }
+        }
+        
+    }
+
+    public void Move()
+    {
+        ballPos = ball.transform.position; // la posicion
+
+        if (transform.position.y > ballPos.y)
+        {
+            transform.position += new Vector3(0, (-speedAI * Time.deltaTime), 0);
+        }
+        if (transform.position.y < ballPos.y)
+        {
+            transform.position += new Vector3(0, (speedAI * Time.deltaTime), 0);
+        }
+        if (transform.position.y >= -7f)
+        {
+            transform.position += new Vector3(0, (-speedAI * Time.deltaTime), 0);
+        }
+        if (transform.position.y <= 7f)
+        {
+            transform.position += new Vector3(0, (speedAI * Time.deltaTime), 0);
+        }
+        
     }
 
     /*public void StartGame()
@@ -40,6 +99,6 @@ public class MovementManager : MonoBehaviour
             transform.position = new Vector3(14f, 0f, 0f);
         }
     }*/
-    
+
 
 }
